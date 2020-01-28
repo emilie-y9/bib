a5b4fa19f6e63d1b8b51d928a0b90ef9bb40aeae
emilie-y9 <emilie.yang298@gmail.com>
2020-01-28 11:52:01 +0100

    feat(michelin): get infos restaurant

--- server/michelin.js
+++ server/michelin.js
@@ -10,9 +10,21 @@ const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
-
-  return {name, experience};
+  const address= $('.restaurant-details__heading > ul > li:nth-child(1)').text();
+  const price= $('div.restaurant-details__heading.d-lg-none > ul > li.restaurant-details__heading-price').text()
+  const street =address.split(',')[0];
+  const city =address.split(',')[1];
+  const postal_code =address.split(',')[2];
+  const state= address.split(',')[3];
+  const tel= $('.section-main span.flex-fill').text().substring(0,17);
+  const feedback = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section.section.section-main.restaurant-details__main > div.restaurant-details__description > div.restaurant-details__description--text.js-show-description > div > p').text()
+  return {name, experience, price, street, city, postal_code,state,tel,feedback};
 };
+
+const parseBibGlist = data => {
+  const $ = cheerio.load(data);
+}
+
 
 /**
  * Scrape a given restaurant url
@@ -25,6 +37,19 @@ module.exports.scrapeRestaurant = async url => {
 
   if (status >= 200 && status < 300) {
     return parse(data);
+  }
+
+  console.error(status);
+
+  return null;
+};
+
+module.exports.scrapeBibGList = async url => {
+  const response = await axios(url);
+  const {data, status} = response;
+
+  if (status >= 200 && status < 300) {
+    return parseBibGList(data);
   }
 
   console.error(status);
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
+const BibG_link='https://guide.michelin.com/fr/fr/ile-de-france/restaurants/bib-gourmand';
 
+//for a particular restaurant
 async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche') {
   try {
     console.log(`🕵️‍♀️  browsing ${searchLink} source`);
@@ -13,6 +15,26 @@ async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-va
   } catch (e) {
     console.error(e);
     process.exit(1);
+  }
+}
+
+//for all restaurants
+async function sandboxBibGList (searchLink = BibGlink) {
+    
+  try {
+    console.log(`🕵️‍♀️  browsing ${searchLink} source`);
+
+    const restaurant = await michelin.scrapeBibGList(searchLink);
+
+    // console.log(restaurant);
+    // console.log('done');
+    
+    return restaurant;
+    // process.exit(0);
+  } catch (e) {
+    // console.error(e);
+    // process.exit(1);
+    return {};
   }
 }
 
